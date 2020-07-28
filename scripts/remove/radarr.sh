d31568ef3605206326fde843bedbd7d3695a858b
Leho <leho@leho.dev>
2020-07-28 11:54:02 +1000

    up
    remove stuff we dont need

--- scripts/remove/radarr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-systemctl stop radarr
-systemctl disable radarr
-rm -rf /etc/systemd/system/radarr.service
-rm -rf /opt/Radarr
-rm -rf /etc/nginx/apps/radarr.conf
-rm -rf /install/.radarr.lock
-echo "Radarr uninstalled!"
--- scripts/remove/syncthing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-MASTER=$(cut -d: -f1 < /root/.master.info)
-systemctl stop syncthing@${MASTER}
-apt-get -q -y purge syncthing
-rm /etc/systemd/system/syncthing@.service
-rm -f  /etc/nginx/apps/syncthing.conf
-systemctl reload nginx
-rm /install/.syncthing.lock
